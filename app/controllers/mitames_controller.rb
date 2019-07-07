a4bbfce9eb0a05e6765a58adfe4fc12b9901653d
EC2 Default User <ec2-user@ip-172-31-16-24.ap-southeast-1.compute.internal>
2019-07-07 08:31:48 +0000

    railsいじったよ

--- app/controllers/mitames_controller.rb
+++ app/controllers/mitames_controller.rb
@@ -1,7 +1,21 @@
 class MitamesController < ApplicationController
   def index
+    @mitames = Mitame.all
   end
   
   def show
   end
+  
+  def new
+  end
+  
+  def create
+    Mitame.create(mitame_params)
+  end
+  
+  private
+  def miatme_params
+    params.permit(:name, :image, :text)
+  end
+
 end
