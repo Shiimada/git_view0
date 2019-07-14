abc6ef7e239057e74af9f39017016713ddc63139
EC2 Default User <ec2-user@ip-172-31-16-24.ap-southeast-1.compute.internal>
2019-07-14 05:08:22 +0000

    newのviewいじ3

--- app/controllers/mitames_controller.rb
+++ app/controllers/mitames_controller.rb
@@ -1,13 +1,12 @@
 class MitamesController < ApplicationController
   # before_action :move_to_index, except: [:index, :show]
-  before_action :set_mitame, only: [ :show, :edit, :update, :destroy]
+  # before_action :set_mitame, only: [ :show, :edit, :update, :destroy]
+ def show
+    @mitame = Mitame.find(params[:id])
+ end
   def index
     @mitames = Mitame.all
   end
-  
-  def show
-  end
-  
   def new
   end
   
